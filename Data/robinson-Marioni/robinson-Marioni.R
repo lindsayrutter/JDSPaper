60a9b9d797ddd0614b83926f347bcec03f24d8a6
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-09-17 10:10:05 -0500

    Marioni raw data plots (log+psuedocount)

--- Data/robinson-Marioni/robinson-Marioni.R
+++ Data/robinson-Marioni/robinson-Marioni.R
@@ -9,13 +9,79 @@ colnames(data) = c("ID","K.R1L1","L.R1L2","K.R1L3","L.R1L4","L.R1L6","K.R1L7","L
 data = as.data.frame(data)
 data = data[,c(1,2,4,7,9,11,3,5,6,8,10)]
 
-# Obtain 2K 2L
-data <- data[,c(1:4,7:9)]
-data[,c(2:ncol(data))] = log(data[,c(2:ncol(data))]+1)
-plotScatterStatic(data)
+baseOutDir = "/Users/lindz/JDSPaper/Data/robinson-Marioni/raw"
 
-# Obtain 2K 2L
-data <- data[,c(1:4,7:9)]
-data[,c(2:ncol(data))] = log(data[,c(2:ncol(data))]+1)
-plotScatterStatic(data)
+# Obtain R1 values for 2K 2L
+outDir = paste0(baseOutDir, "/R1")
+dataSel <- data[,c(1:4,7:9)]
+dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
+plotScatterStatic(dataSel, outDir = outDir)
+boxSel = dataSel[,-1] %>% gather(Sample,Count)
+bPlot = ggplot(boxSel, aes(x=Sample, y=Count)) + geom_boxplot()
+png(filename=paste0(outDir = outDir,"/boxplot.jpg"))
+bPlot
+dev.off()
+plotScatterStatic(dataSel, threshOrth = 0.5, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 1, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 2, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 3, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 4, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99999, outDir = outDir, option="prediction")
+plotScatterInteractive(dataSel, xbins=20)
 
+# Obtain R2 values for 2K 1L
+outDir = paste0(baseOutDir, "/R2")
+dataSel <- data[,c(1,5:6,11)]
+dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
+plotScatterStatic(dataSel, outDir = outDir)
+boxSel = dataSel[,-1] %>% gather(Sample,Count)
+bPlot = ggplot(boxSel, aes(x=Sample, y=Count)) + geom_boxplot()
+png(filename=paste0(outDir = outDir,"/boxplot.jpg"))
+bPlot
+dev.off()
+plotScatterStatic(dataSel, threshOrth = 0.5, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 1, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 2, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 3, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 4, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99999, outDir = outDir, option="prediction")
+plotScatterInteractive(dataSel, xbins=20)
+
+# Obtain K values for R1 and R2
+outDir = paste0(baseOutDir, "/K")
+dataSel <- data[,c(1:6)]
+dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
+plotScatterStatic(dataSel, outDir = outDir)
+boxSel = dataSel[,-1] %>% gather(Sample,Count)
+bPlot = ggplot(boxSel, aes(x=Sample, y=Count)) + geom_boxplot()
+png(filename=paste0(outDir = outDir,"/boxplot.jpg"))
+bPlot
+dev.off()
+plotScatterStatic(dataSel, threshOrth = 0.5, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 1, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99999, outDir = outDir, option="prediction")
+plotScatterInteractive(dataSel, xbins=15)
+
+# Obtain L values for R1 and R2
+outDir = paste0(baseOutDir, "/L")
+dataSel <- data[,c(1,7:11)]
+dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
+
+plotScatterStatic(dataSel, outDir = outDir)
+boxSel = dataSel[,-1] %>% gather(Sample,Count)
+bPlot = ggplot(boxSel, aes(x=Sample, y=Count)) + geom_boxplot()
+png(filename=paste0(outDir = outDir,"/boxplot.jpg"))
+bPlot
+dev.off()
+plotScatterStatic(dataSel, threshOrth = 0.5, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, threshOrth = 1, outDir = outDir, option="orthogonal")
+plotScatterStatic(dataSel, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99, outDir = outDir, option="prediction")
+plotScatterStatic(dataSel, piLevel=0.99999, outDir = outDir, option="prediction")
+plotScatterInteractive(dataSel, xbins=15)
