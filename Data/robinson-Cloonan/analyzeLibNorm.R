207416be446ab9ff826a71232ad91490f448b509
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-09-17 23:13:23 -0500

    Cloonan dataset

---
+++ Data/robinson-Cloonan/analyzeLibNorm.R
@@ -0,0 +1,13 @@
+# This is the data from reference 12 of TMM Robinson paper.
+# It is the data that has been normalized by library size, created in makeDataLibTMM.R
+
+data = readRDS("dataLib.rds")
+
+baseOutDir = "/Users/lindz/JDSPaper/Data/robinson-Cloonan/libNorm"
+
+# Obtain ESEB values
+outDir = paste0(baseOutDir, "/ESEB")
+dataSel <- data
+dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
+plotScatterStatic(dataSel, outDir = outDir)
+
