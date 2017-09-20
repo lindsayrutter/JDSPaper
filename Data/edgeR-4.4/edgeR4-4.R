f275752b904d4025b6a9f4a6b93d4095b4fe4e67
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-09-20 00:02:59 -0500

    edgeR 4.4

--- Data/edgeR-4.4/edgeR4-4.R
+++
@@ -1,6 +0,0 @@
-# This is the data from Section 4.4 of edgeR vignette
-# https://mayoclinic.pure.elsevier.com/en/publications/tumor-transcriptome-sequencing-reveals-allelic-expression-imbalan
-
-library(readxl)
-data <- read_excel("~/JDSPaper/Data/edgeR-4.4/GSE60450_Lactation-GenewiseCounts.xls")
-
