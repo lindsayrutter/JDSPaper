68d1e6dbda5fcec7e19c8da34eb86b75cebbb486
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-09-23 07:09:15 -0500

    within/between DEG plots for yeast data

--- Data/risso-Yeast/deg-AnalyzeWithin.R
+++ Data/risso-Yeast/deg-AnalyzeWithin.R
@@ -1,55 +1,62 @@
-# This is the data from reference 6 of TMM Robinson paper
+# This is the data from EDASeq vignette
 
 library(data.table)
-load("LK_data.RData")
-data = as.data.frame(MA.subsetA$M)
-rownames(data) = as.character(MA.subsetA$genes$EnsemblGeneID)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(edgeR)
+data = readRDS("dataWithin.rds")
 setDT(data, keep.rownames = TRUE)[]
-colnames(data) = c("ID","K.R1L1","L.R1L2","K.R1L3","L.R1L4","L.R1L6","K.R1L7","L.R1L8","K.R2L2","L.R2L3","K.R2L6")
+colnames(data) = c("ID","Y1.1","Y1.2","Y2.1","Y2.2","Y7.1","Y7.2","Y4.1","Y4.2","D.1","D.2","D.7","G.1","G.2","G.3")
 data = as.data.frame(data)
-data = data[,c(1,2,4,7,9,11,3,5,6,8,10)]
 
-outDir = "/Users/lindz/JDSPaper/Data/robinson-Marioni/DEG-tmm"
-
-# Obtain R1 values
-data <- data[,c(1:4,7:9)]
-#dataSel[,c(2:ncol(dataSel))] = log(dataSel[,c(2:ncol(dataSel))]+1)
-
+outDir = "/Users/lindz/JDSPaper/Data/risso-Yeast/DEG-within"
 
 ############# Get DEGs ############# 
 rowNames = data[,1]
 dataSel = data[,-1]
 rownames(dataSel) = rowNames
-group <- factor(c(1,1,1,2,2,2))
+group <- factor(c("Y1","Y1","Y2","Y2","Y7","Y7","Y4","Y4","D","D","D","G","G","G"))
 y <- DGEList(counts=dataSel,group=group)
-y <- calcNormFactors(y)
 design <- model.matrix(~group)
+colnames(design) = levels(group)
 y <- estimateDisp(y, design)
 fit <- glmFit(y,design)
-lrt <- glmLRT(fit,coef=2)
 
-ret = data.frame(ID=rownames(dataSel), lrt[[14]])
-ret$ID = as.character(ret$ID)
-ret = as.data.frame(ret)
-metricList = list()
-metricList[["K_L"]] = ret
+metricList <- list()
+for (i in 1:(ncol(fit)-1)){
+  for (j in (i+1):ncol(fit)){
+    contrast=rep(0,ncol(fit))
+    contrast[i]=1
+    contrast[j]=-1
+    lrt <- glmLRT(fit, contrast=contrast)
+    lrt <- topTags(lrt, n = nrow(y[[1]]))[[1]]
+    
+    setDT(lrt, keep.rownames = TRUE)[]
+    colnames(lrt)[1] = "ID"
+    lrt <- as.data.frame(lrt)
+    
+    metricList[[paste0(colnames(fit)[i], "_", colnames(fit)[j])]] <- lrt
+  }
+}
 
 ############# Create DEG plots #############
 plotData <- data
 plotData[,2:ncol(plotData)] <- log(data[,2:ncol(plotData)]+1)
 
-plotDEG(plotData, metricList, outDir=outDir, threshVar="PValue", threshVal=1e-250)
-plotDEG(plotData, metricList, outDir=outDir, option="volcano", threshVar="PValue", threshVal=1e-250)
-plotDEG(plotData, metricList, outDir=outDir, option="scatterOrthogonal", threshVar="PValue", threshVal=1e-250)
-plotDEG(plotData, metricList, outDir=outDir, option="scatterPrediction", threshVar="PValue", threshVal=1e-250)
-plotDEG(plotData, metricList, outDir=outDir, option="parallelCoord", threshVar="PValue", threshVal=1e-250)
+plotDEG(plotData, metricList, outDir=outDir, threshVar="FDR", threshVal=0.05)
+plotDEG(plotData, metricList, outDir=outDir, threshVar="FDR", threshVal=0.01)
+
+plotDEG(plotData, metricList, outDir=outDir, option = "volcano", threshVar="FDR", threshVal=0.05)
+plotDEG(plotData, metricList, outDir=outDir, option = "volcano", threshVar="FDR", threshVal=0.01)
+
+plotDEG(plotData, metricList, outDir=outDir, option="scatterOrthogonal", threshVar="FDR", threshOrth = 1, threshVal=0.05)
+plotDEG(plotData, metricList, outDir=outDir, option="scatterOrthogonal", threshVar="FDR", threshOrth = 1, threshVal=0.01)
+
+plotDEG(plotData, metricList, outDir=outDir, option="scatterPrediction", threshVar="FDR", threshVal=0.05)
+plotDEG(plotData, metricList, outDir=outDir, option="scatterPrediction", threshVar="FDR", threshVal=0.01)
+
+plotDEG(plotData, metricList, outDir=outDir, option="parallelCoord", threshVar="FDR", threshVal=0.05, lineSize = 0.3)
+plotDEG(plotData, metricList, outDir=outDir, option="parallelCoord", threshVar="FDR", threshVal=0.01, lineSize = 0.3)
 
 plotRepLine(plotData, metricList)
-
-# Statistics
-length(which(lrt[[14]]$PValue<0.1/73320)) # 5704 had p-value <0.1/73320
-length(which(lrt[[14]]$PValue<0.05/73320)) # 5546 had p-value <0.05/73320
-length(which(lrt[[14]]$PValue<0.01/73320)) # 5224 had p-value <0.01/73320
-length(which(lrt[[14]]$PValue<1e-250)) # 365 had p-value <0.01/73320
-
-
