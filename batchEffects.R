7effda362b573ab49a3cdd7372afb48e5001a2e1
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-09-27 11:59:47 -0500

    batch effects simulation

--- batchEffects.R
+++ batchEffects.R
@@ -1,6 +1,7 @@
 # From https://support.bioconductor.org/p/93333/
 library(tidyverse)
 library(genefilter)
+# This cannot be used for scatterplot Matrices
 
 n <- 10000
 m <- 20
@@ -9,14 +10,22 @@ fac <- factor(c(rep(0, 10), rep(1, 10)))
 # Ttests for rows of matrix
 rt1 <- rowttests(x, fac)
 # Data looks okay
-qplot(rt1$p.value, fill = I("tan3"))
+qplot(rt1$p.value, fill = I("tan3")) #batchEffectImage1.png
 
 # Add artificial batch effect by adding 5 to groups 6-10 and 11-15
-x[, 6:15] <- x[, 6:15]+5
+x[, 6:15] <- x[, 6:15]+0.5
 rt2 <- rowttests(x, fac)
-qplot(rt2$p.value, fill = I("coral3"))
+qplot(rt2$p.value, fill = I("coral3")) #batchEffectImage2.png
 
-x = x[,3:8]
-x = as.data.frame(x)
+myData = as.data.frame(x)
+myData = myData[,c(4:9)]
+colnames(myData) = c("N.1","N.2","N.3","B.1","B.2","B.3")
+myData = abs(myData)
+setDT(myData, keep.rownames = TRUE)[]
+colnames(myData)[1] <- "ID"
+myData = as.data.frame(myData)
+plotScatterStatic(myData)
 
+myData[,-1] = log(myData[,-1]+1)
+plotScatterStatic(myData)
 
