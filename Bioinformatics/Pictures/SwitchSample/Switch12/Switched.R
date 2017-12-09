9bc8618f439f2d1665b5928517a24a9695e98d74
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-12-09 12:12:22 -0600

    ASA SLDS tweaks

--- Bioinformatics/Pictures/SwitchSample/Switch12/Switched.R
+++ Bioinformatics/Pictures/SwitchSample/Switch12/Switched.R
@@ -21,13 +21,28 @@ dataSel <- data
 plotScatterStatic(dataSel, outDir = outDir, option ="point")
 plotScatterStatic(dataSel, outDir = outDir)
 boxSel = dataSel[,-1] %>% gather(Sample,Count)
-bPlot = ggplot(boxSel, aes(x=Sample, y=Count)) + geom_boxplot()
-png(filename=paste0(outDir = outDir,"/boxplot.jpg"))
+boxSel$group = c(rep("S1",nrow(boxSel)/2), rep("S2", nrow(boxSel)/2))
+bPlot = ggplot(boxSel, aes(x=Sample, y=Count, fill=group)) + geom_boxplot() + scale_fill_manual(values=c("limegreen","magenta")) + theme(axis.text=element_text(size=12), axis.title=element_text(size=12), legend.position="none")
+png(filename=paste0(outDir = outDir,"/boxplot.jpg"), width=240, height=240)
 bPlot
 dev.off()
 plotScatterStatic(dataSel, threshOrth = 4, outDir = outDir, option="orthogonal")
 plotScatterStatic(dataSel, piLevel=0.99999, outDir = outDir, option="prediction")
-plotMDS(dataSel[,2:ncol(dataSel)])
+
+colors <- c(rep(c("limegreen"), 3), rep(c("magenta"), 3))
+#p <- plotMDS(dataSel[,2:ncol(dataSel)], col=colors, xlab="Dim 1", ylab="Dim 2", labels=colnames(dataSel[,2:ncol(dataSel)]))
+
+plotMDS(dataSel[,2:ncol(dataSel)], col=colors, xlab="Dim 1", ylab="Dim 2", labels=colnames(dataSel[,2:ncol(dataSel)]), xlim=c(-4, 4), ylim=c(-2, 2.5))
+
+tDat <- t(dataSel[,2:ncol(dataSel)])
+datD <- as.matrix(dist(tDat))
+fit <- cmdscale(datD, eig = TRUE, k = 2)
+x <- fit$points[, 1]
+y <- fit$points[, 2]
+qplot(x,y) + geom_text(label = names(x), nudge_y = 0.35, color = 'royalblue', fontface="bold") + labs(x = "Dim 1", y = "Dim 2") + theme(text = element_text(size=12))
+
+
+
 plotScatterInteractive(dataSel, xbins=20)
 
 ############# Get DEGs ############# 
