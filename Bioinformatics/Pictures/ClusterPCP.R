5fb74e3086023dc38cceeec5e8143ef46eec5266
Lindsay Rutter <lindsayannerutter@gmail.com>
2018-02-04 12:18:03 -0600

    Streak DEGs PCP

--- Bioinformatics/Pictures/ClusterPCP.R
+++ Bioinformatics/Pictures/ClusterPCP.R
@@ -9,94 +9,31 @@ data(soybean_ir_metrics)
 metrics <- soybean_ir_metrics
 metrics <- metrics[["N_P"]]
 metrics <- metrics[with(metrics, order(PValue)), ]
-
 sbLog = soybean_ir
-sbLog[,2:ncol(sbLog)] <- log(sbLog[,2:ncol(sbLog)]+1)
-
+sbLog[,-1] <- log(sbLog[,-1]+1)
 soybean_ir_metrics[["N_P"]]$ID <- as.character(soybean_ir_metrics[["N_P"]]$ID)
-# Plot DEGs on scatMat
-plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue")
-plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue", threshVal = 0.05/nrow(sbLog))
-plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue", threshVal = 0.01/nrow(sbLog))
+
+outDir = "Clustering_data2"
+
+#13771 DEGs p-value<0.05
+plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue", fileName=paste(getwd(), "/", outDir, "/", "SM_DEG_PVal_0.05.jpg", sep=""))
+
+# 2050 DEGs FDR<0.05
+plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue", threshVal = 0.05/nrow(sbLog), fileName=paste(getwd(), "/", outDir, "/", "SM_DEG_FDR_0.05.jpg", sep=""))
+
+# 1681 DEGs FDR<0.01
+plotDEG(sbLog, soybean_ir_metrics, option="scatterPoints", threshVar = "PValue", threshVal = 0.01/nrow(sbLog), fileName=paste(getwd(), "/", outDir, "/", "SM_DEG_FDR_0.01.jpg", sep=""))
 
 #### Determine the five streak genes (all have PValues between 0.58 and 0.90)
-# Glyma.18G057100.Wm82.a2.v1
-# Glyma.18G092200.Wm82.a2.v1
-# Glyma.09G164000.Wm82.a2.v1
-# Glyma.08G269900.Wm82.a2.v1
-# Glyma.07G079300.Wm82.a2.v1
 streakIDs = which(metrics$ID %in% c("Glyma.18G057100.Wm82.a2.v1", "Glyma.18G092200.Wm82.a2.v1", "Glyma.09G164000.Wm82.a2.v1", "Glyma.08G269900.Wm82.a2.v1", "Glyma.07G079300.Wm82.a2.v1"))
-metrics[streakIDs,]
+metrics$PValue = 1e-10
+streakMetrics = metrics[streakIDs,]
+metrics = list()
+metrics[["N_P"]] = streakMetrics
 
-L120 = soybean_ir
-RowSD = function(x) {
-  sqrt(rowSums((x - rowMeans(x))^2)/(dim(x)[2] - 1))
-}
-L120 = mutate(L120, mean = (N.1+N.2+N.3+P.1+P.2+P.3)/6, stdev = RowSD(cbind(N.1,N.2,N.3,P.1,P.2,P.3)))
-L120q1 = L120[,2:7]
-L120q1s = t(apply(as.matrix(L120q1), 1, scale))
-data = L120q1s
-data = as.data.frame(data)
-data$ID = L120$ID
-data = data[,c(7,1,2,3,4,5,6)]
-colnames(data) = c("ID","N.1","N.2","N.3","P.1","P.2","P.3")
+plotDEG(data=sbLog, dataMetrics=metrics, threshVar = "PValue", outDir=outDir, option="parallelCoord", fileName=paste(getwd(), "/", outDir, "/", "N_P_Streak_SM.jpg", sep=""), lineSize=0.5)
 
-baseOutDir = "Clustering_N_P_Top100"
-id100 = metrics$ID[1:100]
-id100df = which(data$ID %in% id100)
-data = data[id100df,]
+soyScale = soybean_ir
+soyScale[,-1] <- t(apply(as.matrix(soybean_ir[,-1]), 1, scale))
 
-plotName = "N_P"
-outDir= paste0(getwd(),"/",baseOutDir)
-
-dendo = data[,-1]
-rownames(data[,-1]) = NULL
-# Euclidean distance between rows of matrix
-d = dist(as.matrix(dendo))
-# Hierarchical clustering using ward.D linkage
-hc = hclust(d, method="ward.D")
-plotName = "N_P_Dendogram"
-jpeg(file = paste(outDir, "/", plotName, ".jpg", sep=""))
-plot(hc,main=plotName, xlab=NA, sub=NA)
-invisible(dev.off())
-
-getPCP <- function(nC, hc, data, outDir){
-  
-  boxDat <- data %>% gather(key, val, c(-ID))
-  colnames(boxDat) <- c("ID", "Sample", "Count")
-  
-  colList = c("red","magenta", "darkorange","darkgreen","blue","purple")
-  k = cutree(hc, k=nC)
-  ###########################
-  plot_clusters = lapply(1:nC, function(i){
-    plotName = "N_P"
-    x = as.data.frame(data[which(k==i),])
-    nGenes = nrow(x)
-    x$cluster = "color"
-    x$cluster2 = factor(x$cluster)
-    xNames = rownames(x)
-    write.table(xNames, file = paste(outDir, "/", plotName, "_", nC, "_", i, ".txt", sep=""), sep=",", row.names=FALSE, col.names=FALSE, quote=FALSE)  
-    
-    xPCP <- x[,c(1:7)]
-    pcpDat2 <- xPCP %>% gather(key, val,c(-ID))
-    colnames(pcpDat2) <- c("ID", "Sample", "Count")
-    p <- ggplot(boxDat, aes_string(x = 'Sample', y = 'Count')) + geom_boxplot() + geom_line(data=pcpDat2, aes_string(x = 'Sample', y = 'Count', group = 'ID'), color = colList[i]) + xlab(paste("Cluster ", i, " (n=", format(nGenes, big.mark=",", scientific=FALSE), ")",sep="")) + theme(legend.position = "none", axis.title=element_text(size=12), axis.text=element_text(size=15), axis.text.x = element_text(angle = 90, hjust = 1, size=15), axis.text.y = element_text(size=15), axis.title.x = element_text(size=15), axis.title.y = element_text(size=15))
-    fileName = paste(outDir, "/", plotName, "_", nC, "_", i, ".jpg", sep="")
-    jpeg(fileName)
-    plot(p)
-    invisible(dev.off())
-    p    
-  })
-  ###########################
-  jpeg(file = paste(outDir, "/N_P_", nC, ".jpg", sep=""), height = 200 * ceiling((nC+1)/3), width = min(200 * (nC+1), 800))
-  # We allow up to 4 (now 3) plots in each column
-  p = do.call("grid.arrange", c(plot_clusters, ncol=3)) #change from 4 to 3 ceiling(nC/3)
-  invisible(dev.off())
-}
-
-data[,2:7] = log(data[,2:7]+1)
-
-for (i in c(2,3,4,5,6)){
-  getPCP(nC=i, hc=hc, data=data, outDir=outDir)
-}
-
+plotDEG(data=soyScale, dataMetrics=metrics, threshVar = "PValue", outDir=outDir, option="parallelCoord", fileName=paste(getwd(), "/", outDir, "/", "N_P_Streak_SM_Scaled.jpg", sep=""), lineSize=0.5)
