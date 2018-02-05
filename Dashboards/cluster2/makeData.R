04ef695eb5e9d566d3fa0f137f46a4652689cf60
Lindsay Rutter <lindsayannerutter@gmail.com>
2018-02-05 10:26:27 -0600

    Dashboard for hiearchical clusters 2, 3

---
+++ Dashboards/cluster2/makeData.R
@@ -0,0 +1,15 @@
+library(bigPint)
+
+ID <- readRDS("../../Bioinformatics/Pictures/Clustering_data/FocusOn/Sig_4_2.Rds")
+load("soybean_ir_metrics.rda")
+
+allMetrics = soybean_ir_metrics[["N_P"]]
+#allMetrics$PValue = 1
+
+metricsCluster <- allMetrics[which(allMetrics$ID %in% ID),]
+metricsCluster$ID <- as.character(metricsCluster$ID)
+
+metrics <- list()
+metrics[["N_P"]] <- metricsCluster
+
+save(metrics, file = "cluster2_metrics.rda")
